dbb051680bb0ada2022f809c963b9d9d33dd009c
NatPuff <cesarp102104@gmail.com>
2021-11-10 09:15:51 -0600

    Initial Commit

---
+++ WeatherApp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp2
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp2/WeatherApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2App.swift
+//  WeatherApp2
+//
+//  Created by AMStudent on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
